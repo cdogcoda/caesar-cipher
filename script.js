abf2c5a5c085d11abe7839cc8ff6687aef23e47f
CDog <cdog97456@gmail.com>
2024-04-21 21:12:51 -0700

    Create function shiftMessage to shift message

--- script.js
+++ script.js
@@ -4,4 +4,18 @@
 // -> Returns the new message to the function call
 // -> Called when submit button is pressed
 // Create function to display the message in the textbox
-// -> Should also clear the message and shift box contents 
+// -> Should also clear the message and shift box contents
+
+let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+function shiftMessage(message, shiftAmount) {
+    let shiftedMessage = "";
+    let currentLetterIndex;
+    let shiftedLetterIndex; 
+    for (const msgLetter of message) {
+        currentLetterIndex = alphabet.findIndex((letter) => letter == msgLetter);
+        shiftedLetterIndex = currentLetterIndex+shiftAmount;
+        shiftedMessage += alphabet[shiftedLetterIndex];
+    }
+    return shiftedMessage;
+}
