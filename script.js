db749c18ab822e3c02b98ec78c9c7b5335492d9a
CDog <cdog97456@gmail.com>
2024-04-21 20:59:05 -0700

    Add pseudo starter code to script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// My Pseudocode
+
+// Create function to receive message and shift 
+// -> Returns the new message to the function call
+// -> Called when submit button is pressed
+// Create function to display the message in the textbox
+// -> Should also clear the message and shift box contents 
